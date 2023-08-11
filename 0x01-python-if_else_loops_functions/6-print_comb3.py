b7787548ec301a5e1d14cd097477cfcc9963c87a
TheGreatestbug <izyspikes@gmail.com>
2023-08-11 01:22:43 +0100

    Tricky combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for fir_dig in range(10):
     for sec_dig in range (fir_dig + 1,10):
         if fir_dig < 8:
-            print('{}{}'.format(fir_dig, sec_dig), end=' ')
+            print('{}{}'.format(fir_dig, sec_dig), end=', ')
         else:
             print('{}{}'.format(fir_dig, sec_dig))
