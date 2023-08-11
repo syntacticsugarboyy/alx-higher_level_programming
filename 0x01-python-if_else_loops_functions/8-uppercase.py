e8eb7e96ff48a2f7dfc04078403f1e955ba36b58
TheGreatestbug <izyspikes@gmail.com>
2023-08-11 03:08:35 +0100

    Prints lowercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
-            print('{}'.format(chr(ord(letter) - 32)), end='')
-        else:
-            print('{}'.format(letter), end='')
+            letter = chr(ord(letter) - 32)
+        print('{}'.format(letter), end='')
     print('')
