bec3a7036528d4466906ea9d6ca18b5cb21a6977
TheGreatestbug <izyspikes@gmail.com>
2023-08-29 23:43:06 +0100

    Creates a class with an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+''' Square
+    Author: syntactic_sugarboyy
+'''
+
+class Square:
+    ''' Creates a class and an attribute '''
+    def __init__(self, size):
+        self.__size = size
