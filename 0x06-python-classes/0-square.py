ab8ce48bdb973ee5453deb9e6668d8f5410b2cfd
TheGreatestbug <izyspikes@gmail.com>
2023-08-29 23:11:07 +0100

    Creates a class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-class Square(object):
+# -*- coding: utf-8  -*-
+''' Square Class
+    Author: syntactic_sugarboyy
+'''
+class Square:
+    '''
+    An Empty Class
+    '''
     pass
