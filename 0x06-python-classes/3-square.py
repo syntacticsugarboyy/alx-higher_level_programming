a28c10b3c60fcdba8d643ea28f679357d764cc6f
TheGreatestbug <izyspikes@gmail.com>
2023-08-30 00:25:48 +0100

    Calculates the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,13 +16,14 @@ class Square:
             Raises ValueError if size is < 0
     '''
     def __init__(self, size=0):
-        if type(size) == int:
+        if isinstance(size, int):
             if size >= 0:
                 self.__size = size
             else:
                 raise ValueError('size must be >= 0')
         else:
             raise TypeError('size must be an integer')
+
     def area(self):
         '''
             Returns the area of the Square
