7e5c16d567a8a15299b79e8922dbc7ce0da8b15f
TheGreatestbug <izyspikes@gmail.com>
2023-09-11 23:31:21 +0100

    dir()

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+    lookup
+    implements the dir() function
+'''
+
+def lookup(obj):
+    '''
+        Returns the list of available attributes and methods of an object
+    '''
+    dir_list = dir(object)
+    return dir_list
