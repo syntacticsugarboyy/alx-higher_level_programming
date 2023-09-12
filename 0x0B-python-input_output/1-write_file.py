81abd65036fb1af9e081cefa778590e6acc1337b
TheGreatestbug <izyspikes@gmail.com>
2023-09-13 00:13:18 +0100

    Writes text into a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,7 +7,5 @@
 
 def write_file(filename="", text=""):
     ''' writes text into a file '''
-    if not filename:
-        return
     with open('filename', 'w', encoding='utf-8') as file:
         return file.write(text)
