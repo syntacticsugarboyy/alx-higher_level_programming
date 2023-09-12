8db55ff9b089b2d41e03c105df4eff0412f43b65
TheGreatestbug <izyspikes@gmail.com>
2023-09-12 02:32:15 +0100

    Application of super()

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,28 +2,7 @@
 '''
     BaseGeometry
 '''
-
-
-class BaseGeometry:
-    '''
-        A parent Class named BaseGeometry
-    '''
-    def area(self):
-        '''
-            Raises:
-                Exception
-        '''
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''
-            Validates value
-        '''
-        if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-        self.name = value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
