92d3dab7715795705135a3108eadff21078de0bd
TheGreatestbug <izyspikes@gmail.com>
2023-09-13 13:33:12 +0100

    getattr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    Student to JSON
+'''
+
+
+class Student:
+    '''
+        a class named Student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+            Initializes a Student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+            Grabs Student details
+        '''
+        if attrs is None:
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
