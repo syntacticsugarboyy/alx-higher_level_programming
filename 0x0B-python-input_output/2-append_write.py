b2889f762c922486237aca2b05c649cae5e7ec87
TheGreatestbug <izyspikes@gmail.com>
2023-09-13 01:40:38 +0100

    appends

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
     '''
         Appends text to a file
     '''
-    with open('filename', 'a', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
