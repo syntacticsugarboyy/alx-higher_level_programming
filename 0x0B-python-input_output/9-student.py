c0bb27015a3461ab6b0afedb9133fd7731064a09
TheGreatestbug <izyspikes@gmail.com>
2023-09-13 01:47:26 +0100

    Class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/[ython3
+'''
+    Student to JSON
+'''
+
+
+class Student:
+    '''
+        a class named Student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+            Initializes a Student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+            Grabs Student details
+        '''
+        return self.__dict__
