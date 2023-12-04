9918784cc59fa1a2d5fd898d618eec36d9e6560d
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-04 23:13:49 +0000

    Validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,10 +17,9 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         '''
-            Indicates Value
+            Validates value
         '''
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
-        self.name = value
