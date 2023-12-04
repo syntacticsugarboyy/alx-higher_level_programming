4211820a3e0caf86f7cbf9229896b3ec082db2e4
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-04 15:50:57 +0000

    Raises Geometric exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+     Improve Geometry
+'''
+
+
+class BaseGeometry:
+    '''
+        Shapes
+    '''
+    def area(self):
+        '''
+            Raises an exception
+        '''
+        raise Exception('area() is not implemented')
