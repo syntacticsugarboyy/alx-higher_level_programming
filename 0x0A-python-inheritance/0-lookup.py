99187b1e58b4f63413ea8ea5135f23f024fdfaa2
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-04 10:39:36 +0000

    dir()

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+    Lookup
+'''
+
+
+def lookup(obj):
+    '''
+        Returns list of available attributes and methods of an object.
+    '''
+    return (dir(obj))
