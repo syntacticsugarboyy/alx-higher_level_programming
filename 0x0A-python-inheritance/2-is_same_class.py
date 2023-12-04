e53190d7f0205a87b8e0e0d1edddafcf45c2f48a
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-04 11:02:14 +0000

    Checks for exactly same objects

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+# -*- coding: utf-8 -*-
+'''
+    Exact Same Object
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+        Checks if an object is an instance of the specifies class
+    '''
+    if type(obj) == a_class:
+        return True
+    return False
