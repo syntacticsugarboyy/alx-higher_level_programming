fce53b49942893cf4093587e513e57e8d430900b
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-05 22:33:00 +0000

    attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+    Student to JSON
+'''
+
+
+class Student:
+    '''
+        Student to JSON
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+            Instantiation
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+            Dictionary repr of Student
+        '''
+        if isinstance(attrs, list) and all (isinstance(elem, str) for elem in attrs):
+            return ({k: getattr(self, k) for k in attrs if hasattr(self, k)})
+        return (self.__dict__)
