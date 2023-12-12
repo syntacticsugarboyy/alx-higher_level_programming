6974e1f2590a917dfd71ec30833d85929a135022
syntacticsugarboyy <izyspikes@gmail.com>
2023-12-12 16:02:08 +0000

    Setter validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,8 +20,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''
-            Sets width
+            Sets and validates width
         '''
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -34,8 +38,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         '''
-            Sets height
+            Sets and validates height
         '''
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
         '''
             Sets x
         '''
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -64,6 +76,10 @@ class Rectangle(Base):
         '''
             Sets y
         '''
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
 
     def __init__(self, width, height, x=0, y=0, id=None):
